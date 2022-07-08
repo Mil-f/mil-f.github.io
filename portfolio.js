b80b988cd690e12361ab179274ddbd2b5c91ad1a
Mil-f <95150346+Mil-f@users.noreply.github.com>
2022-07-08 09:25:47 +0200

    yup yup

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    var typed = new Typed("#description", {
+            strings: ["So you're looking for a programmer, <br> someone who can make your dream website into a reality-- <br> well look no further! For I can do that for you. <br>"], 
+            typeSpeed: 0,
+            backSpeed: 0,
+            backDelay: 500,
+            startDelay: 1000,
+            loop: false,
+            cursorChar: "",
+        });
+    } 
+    
